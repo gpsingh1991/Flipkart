2c7e61abfa105d47686da1bf15fd17428857087a
sonalpathak <sonalpathak33@gmail.com>
2013-09-13 16:50:24 +0530

    initial commit with source code

---
+++ modal.js
@@ -0,0 +1,80 @@
+<script type="text/javascript">
+function table_creation(){
+	//creating table_user
+	//creating caterogy_table
+	//creating  item_table
+	//creating  cart_table
+	//creating order_table
+}
+function register_click(){
+	// create table users
+	//onsuccessful creation
+}
+function registerUser(){
+	//inserting into tables
+
+}
+function count_category_clicked(){
+// 	if(count==0)call category_clicked();
+ }
+
+function load_category(){
+	///inserting categories
+		//fetching data from table
+}
+function category_clicked(){
+
+//select category id clicked and display.
+//onsuccess load_item_list()
+}
+function category_display(){
+	//this function is called whendrop down is pressed
+	//select  category_id from categorytable;
+
+}
+function load_item_list(){
+	//inserting data into tables
+	//if(item>0)
+	//call items_list
+}
+function items_list(){
+	//this function is called when a particular item is clicked
+}
+function cart_table(){
+	//called when user clicks on buy
+	//checks if the user has logged in 
+	//insert into cart_table
+	//on success create table cart_details 
+}
+function cart_details(){
+	//fetches cart_id,item_id,quantity price
+	//if quantity is updated
+
+
+	update_card_table(cart_id);
+}
+function update_cart_table(cart_id){
+// 	select * from cart_details where cart_id=this.cart_id;//here we get the array of items
+// 	var Qty=0 ,price=0;
+// 	for( )
+// 	{
+// 		Qty+=qty;
+// 		similarly increment the price;
+// 	}
+// update cart_table set qty=qty and price=.. where cart_id=this.cart_id;
+}
+function order_clicked(){
+	//called when user clicks on place_order
+	//checks if the user has logged in
+    //calls the order_details
+
+}
+function order_details(){
+	//creats table and fetches cartid, order id etc 
+	//similarly when the update operation is performed call update_clicked()
+}
+
+
+</script>
+
+//on window.load() 
